6b488248dc6071f39504127624b51d6c6bd1f413
Claessen <Guus.Claessen@sweco.nl>
2017-12-05 14:00:11 +0100

    Trivia mag rijmen

--- src/app/trivia-items.ts
+++ src/app/trivia-items.ts
@@ -3,7 +3,7 @@ import { TriviaItem } from './trivia/TriviaItem';
 export const TRIVIA_ITEMS: TriviaItem[] = [
   {
     title: 'Onbewoond',
-    question: 'Welk eiland wordt hier aangewezen?',
+    question: 'Al dobberend in de oceaan, welk eiland wijst deze persoon hier aan?',
     answer: 'Tenerife',
     image: 'mysteryIsland.jpg',
     code: '3264',
@@ -11,7 +11,7 @@ export const TRIVIA_ITEMS: TriviaItem[] = [
   },
   {
     title: 'Verschil',
-    question: 'In welke maand is Delta geboren?',
+    question: 'Fluffy liggend of staand, Delta werd geboren in welke maand?',
     answer: 'Maart',
     image: 'deltaSlaapt.jpg',
     code: '5648',
@@ -19,14 +19,15 @@ export const TRIVIA_ITEMS: TriviaItem[] = [
   },
   {
     title: 'Boarding',
-    question: 'Wat is het bekendste Snowboardmerk?',
-    answer: 'Burton',
+    question: 'De naam ervan is een samengevoegd woord, wat was de voorganger van de snowboard?',
+    answer: 'Snurfer',
     code: '8429',
+    image: 'snowboard-pre-retro.jpg',
     input: ''
   },
   {
     title: 'Chanel',
-    question: 'In welk jaar werd Chanel opgericht?',
+    question: 'N°5 zag in de roaring twenties het licht, maar in welk jaar werd het modehuis opgericht?',
     answer: '1910',
     code: '6286',
     image: 'coco.jpg',
@@ -34,7 +35,7 @@ export const TRIVIA_ITEMS: TriviaItem[] = [
   },
   {
     title: 'Vulkanisch',
-    question: 'Welke vulkaan zien we hier?',
+    question: 'Je ziet hem van ver al staan, maar hoe heet deze vulkaan?',
     answer: 'Kronotsky',
     code: '3419',
     image: 'mysteryVolcano.jpg',
